a346ff1868abd9fc836117afec93acb968ae66d4
Nick Watson <volodarskyns@gmail.com>
2015-07-21 10:27:36 +0300

    Задача с баноком

---
+++ Bank/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Думаем над решением
+
+    int N, S, i;
+    cin >>N;
+    int x[N];
+
+
+
+    for (i=0; i<N; i++)
+    {
+        cin >>x[i];
+    }
+
+    cin >>S;
+
+
+
+
+    return 0;
+}
